e2d91fa95607c3c3d147ddda69f17e847bbc49bf
Rishi Suresh <rishi9632@gmail.com>
2020-05-24 23:34:26 +0530

    modify index.html to support github pages

---
+++ docs/precache-manifest.1382ce937686321052d733f72ca2ce7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5d03d9468cb0fd013cffa6470de12b",
+    "url": "/Shopping-react/index.html"
+  },
+  {
+    "revision": "121f8b2ca58caf63fa8c",
+    "url": "/Shopping-react/static/css/main.42844da4.chunk.css"
+  },
+  {
+    "revision": "aa147a741f91550e5012",
+    "url": "/Shopping-react/static/js/2.0a9520c9.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/Shopping-react/static/js/2.0a9520c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121f8b2ca58caf63fa8c",
+    "url": "/Shopping-react/static/js/main.8039c658.chunk.js"
+  },
+  {
+    "revision": "6b271f7b6da1c4517858",
+    "url": "/Shopping-react/static/js/runtime-main.5b12059b.js"
+  },
+  {
+    "revision": "abf2bd4a1e9233f012bc0602d7f03927",
+    "url": "/Shopping-react/static/media/reuse.abf2bd4a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Shopping-react/static/media/shopping-bag.30f07071.svg"
+  }
+]);
